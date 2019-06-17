0784fec598da3551a9b60b5bc0106d20496219b8
KaustubhD <kaustubhd287@gmail.com>
2019-06-17 23:06:32 +0530

    First commit Week 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Toolbox
+
+This is a course on Coursera. Check it out [here](https://www.coursera.org/learn/algorithmic-toolbox?).
+
+I audited the course, so keeping the assignments here in case i do pay for the course.
+
+1. Week 1
+    * [Add two numbers]()
+    * [Maximum Pairwise Product]()
---
+++ Week1/2_maximum_pairwise_product/MaxPairwiseProduct.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+
+public class MaxPairwiseProduct {
+    static int getMaxPairwiseProduct(int[] numbers) {
+        int max_product = 0;
+        int n = numbers.length;
+        int largest = -1, sec_largest = -1;
+
+        for (int first = 0; first < n; ++first) {
+          if(largest == -1 || numbers[first] > numbers[largest])
+            largest = first;
+        }
+
+        for (int second = 0; second < n; ++second) {
+          if(second != largest && (sec_largest == -1 || numbers[second] > numbers[sec_largest]))
+            sec_largest = second;
+        }
+
+        return numbers[largest] * numbers[sec_largest];
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                    InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
